fa0292dcdf9cd61e7276e2f5d0eefc576e15ed48
netrounds-aarish <aarish.zaveri@netrounds.com>
2023-05-23 08:59:32 +0200

    added db requirements

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """
+    Add x and y, return the result
+    """
+    return x + y
+
+def subtract(x, y):
+    """
+    Subtract y from x, return the result
+    """
+    return x - y
