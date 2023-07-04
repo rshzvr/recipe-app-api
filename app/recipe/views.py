61fa028cd4c94dc615cea7697a442a43e2f37acd
netrounds-aarish <aarish.zaveri@netrounds.com>
2023-07-04 12:22:37 +0200

    section 14

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+"""
+Views for the recipe APis
+"""
 
-# Create your views here.
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe, Tag
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """
+    View for manage recipe APIs
+    """
+    serializer_class = serializers.RecipeDetailSerializer
+    # here is defined the objects that are managed through this API
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Retrieve recipes for authenticated user
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """
+        Return the serializer class for request
+        """
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        else:
+            return self.serializer_class
+
+    def perform_create(self, serializer):
+        """
+        Create a new recipe
+        """
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    View for manage tag APIs
+    """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Retrieve recipes for authenticated user
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-id')
